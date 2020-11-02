452b722d99e82d8389413791d0555fc88bc3c075
WangNing <justdoitwn@163.com>
2020-11-02 15:14:54 +0800

    1. Add ApiProfiles table and relevant logic code;  2. Update Api GET /ams/v1/product support finding compatible managed apps of one runtime engine product

---
+++ ibroker/ams-api-gateway/src/main/java/com/intel/iot/ams/repository/ApiProfilesDao.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 Intel Corporation. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.intel.iot.ams.repository;
+
+import com.intel.iot.ams.entity.ApiProfiles;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApiProfilesDao extends JpaRepository<ApiProfiles, Integer> {
+
+  public List<ApiProfiles> findByProductNameAndProductVersion(String productName, String productVersion);
+
+}
